64b4fa8d5868456e661fca61abd064371633fe48
jose luis sanchez <muallin@gmail.com>
2016-02-21 12:46:50 +0100

    used provided gradle daemon

--- build-all.sh
+++ build-all.sh
@@ -2,18 +2,18 @@
 
 set -e
 
-cd util;                                              ./gradlew clean build publishToMavenLocal; cd -
+cd util;                                              gradle --daemon --parallel clean build publishToMavenLocal; cd -
 
-cd microservices/core/product-service;                ./gradlew clean build buildDockerImage; cd -
-cd microservices/core/recommendation-service;         ./gradlew clean build buildDockerImage; cd -
-cd microservices/core/review-service;                 ./gradlew clean build buildDockerImage; cd -
-cd microservices/composite/product-composite-service; ./gradlew clean build buildDockerImage; cd -
+cd microservices/core/product-service;                gradle --daemon --parallel clean build buildDockerImage; cd -
+cd microservices/core/recommendation-service;         gradle --daemon --parallel clean build buildDockerImage; cd -
+cd microservices/core/review-service;                 gradle --daemon --parallel clean build buildDockerImage; cd -
+cd microservices/composite/product-composite-service; gradle --daemon --parallel clean build buildDockerImage; cd -
 
-cd microservices/support/auth-server;                 ./gradlew clean build buildDockerImage; cd -
-cd microservices/support/config-server;               ./gradlew clean build buildDockerImage; cd -
-cd microservices/support/discovery-server;            ./gradlew clean build buildDockerImage; cd -
-cd microservices/support/edge-server;                 ./gradlew clean build buildDockerImage; cd -
-cd microservices/support/monitor-dashboard;           ./gradlew clean build buildDockerImage; cd -
-cd microservices/support/turbine;                     ./gradlew clean build buildDockerImage; cd -
+cd microservices/support/auth-server;                 gradle --daemon --parallel clean build buildDockerImage; cd -
+cd microservices/support/config-server;               gradle --daemon --parallel clean build buildDockerImage; cd -
+cd microservices/support/discovery-server;            gradle --daemon --parallel clean build buildDockerImage; cd -
+cd microservices/support/edge-server;                 gradle --daemon --parallel clean build buildDockerImage; cd -
+cd microservices/support/monitor-dashboard;           gradle --daemon --parallel clean build buildDockerImage; cd -
+cd microservices/support/turbine;                     gradle --daemon --parallel clean build buildDockerImage; cd -
 
 find . -name *SNAPSHOT.jar -exec du -h {} \;
